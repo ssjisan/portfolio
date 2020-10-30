f12d19ef15addf19e605e4d5bf0b4aaab4f84fee
ssjisan <sakibfset13@gmail.com>
2020-10-30 21:50:15 +0600

    skill

--- src/Components/Home/Skill/Skill.js
+++ src/Components/Home/Skill/Skill.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Skill.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faHtml5,faFacebookF,faMediumM,faGithub } from '@fortawesome/free-brands-svg-icons'
+import {  faHtml5,faCss3Alt,faBootstrap,faJsSquare,faReact ,faGithub } from '@fortawesome/free-brands-svg-icons'
 const Skill = () => {
     return (
         <div class="fixed-bg">
@@ -11,11 +11,11 @@ const Skill = () => {
                     <div class="subheading">Programming Languages & Tools</div>
                     <ul class="list-inline dev-icons">
                         <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
+                        <li class="list-inline-item"><FontAwesomeIcon icon={faCss3Alt} /></li>
+                        <li class="list-inline-item"><FontAwesomeIcon icon={faBootstrap} /></li>
+                        <li class="list-inline-item"><FontAwesomeIcon icon={faJsSquare} /></li>
+                        <li class="list-inline-item"><FontAwesomeIcon icon={faReact} /></li>
+                        <li class="list-inline-item"><FontAwesomeIcon icon={faGithub} /></li>
                         <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
                         <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
                         <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
