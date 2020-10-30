b667e6e1dac99bbca59dc13feb01efa21124db84
ssjisan <sakibfset13@gmail.com>
2020-10-30 22:04:14 +0600

    skills fixed

--- src/Components/Home/Skill/Skill.js
+++ src/Components/Home/Skill/Skill.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Skill.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faHtml5,faCss3Alt,faBootstrap,faJsSquare,faReact ,faGithub } from '@fortawesome/free-brands-svg-icons'
+import {  faHtml5,faCss3Alt,faBootstrap,faJsSquare,faReact ,faGithub, faNodeJs, faNpm } from '@fortawesome/free-brands-svg-icons'
 const Skill = () => {
     return (
-        <div class="fixed-bg">
+        <div class="fixed-bg area">
             <section class="resume-section" id="skills">
                 <div class="resume-section-content">
                     <h2 className="headings">Skills</h2>
@@ -16,12 +16,8 @@ const Skill = () => {
                         <li class="list-inline-item"><FontAwesomeIcon icon={faJsSquare} /></li>
                         <li class="list-inline-item"><FontAwesomeIcon icon={faReact} /></li>
                         <li class="list-inline-item"><FontAwesomeIcon icon={faGithub} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
+                        <li class="list-inline-item"><FontAwesomeIcon icon={faNodeJs} /></li>
+                        <li class="list-inline-item"><FontAwesomeIcon icon={faNpm} /></li>
                     </ul> 
                 </div>
             </section>
