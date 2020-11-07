1b2bce493ab7b08291ce50ddb3cea0e497844914
ssjisan <sakibfset13@gmail.com>
2020-11-07 11:19:10 +0600

    edited

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './About.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import image from './images/image.png'
 const About = () => {
+    useEffect(()=>{
+        AOS.init({duration:2000})
+    }, [])
     return (
         <div className="about">
             <div className="container">
                 <div class="row align-items-center container">
-                    <div class="col-md-6">
+                    <div class="col-md-6" data-aos="zoom-in-up">
                         <img src={image} class="d-block w-100"  alt=""/>
                     </div>
-                    <div class="col-md-6">
-                        <h1>About <span style={{color:"#C7EA46"}}>Myself</span> </h1>
-                        <p>Trying a new thing is fun. New technologies, new ideas. I love to work with versatility. At a very early age at my university, I tried several things. Firstly I was trying to work with robotics. Then work with graphics designing, design game assets, animation, etc. My final year's project was developing a First-Person Shooting game. I worked as an asset designer in this team. I love learning about new technologies. Now I start learning Networking related staff. Cisco Routing-Switching, Mikrotik Routing. For almost two years, I worked as a network engineer in an Internet service provider(ISP). But working as a developer was my passion at the very early stage of my career. I have a little knowledge of HTML5, CSS3. So I start learning about front-end development. Last six months, I learned about JavaScript, ReactJS, NodeJS, MongoDB, Bootstrap. And prepare me as a junior or entry-level front-end developer. 
-
-</p>
+                    <div class="col-md-6" data-aos="zoom-in-down">
+                        <h1 style={{textAlign:"center"}}>About <span style={{color:"#C7EA46"}}>Myself</span> </h1>
+                        <p style={{fontSize:30}}> Observe, Learn and Improve. Trying to learn new thing everyday. </p>
                     </div> 
                 </div>
             </div>
--- src/Components/Home/Skill/Skill.js
+++ src/Components/Home/Skill/Skill.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Skill.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {  faHtml5,faCss3Alt,faBootstrap,faJsSquare,faReact ,faGithub, faNodeJs, faNpm } from '@fortawesome/free-brands-svg-icons'
 const Skill = () => {
+    useEffect(()=>{ 
+        AOS.init({duration:2000})
+    }, [])
     return (
         <div class="fixed-bg area">
             <section class="resume-section" id="skills">
@@ -10,14 +15,14 @@ const Skill = () => {
                     <h2 className="headings">Skills</h2>
                     <div class="subheading">Programming Languages & Tools</div>
                     <ul class="list-inline dev-icons">
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faHtml5} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faCss3Alt} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faBootstrap} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faJsSquare} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faReact} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faGithub} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faNodeJs} /></li>
-                        <li class="list-inline-item"><FontAwesomeIcon icon={faNpm} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-up"><FontAwesomeIcon icon={faHtml5} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-down"><FontAwesomeIcon icon={faCss3Alt} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-up"><FontAwesomeIcon icon={faBootstrap} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-down"><FontAwesomeIcon icon={faJsSquare} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-up"><FontAwesomeIcon icon={faReact} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-down"><FontAwesomeIcon icon={faGithub} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-up"><FontAwesomeIcon icon={faNodeJs} /></li>
+                        <li class="list-inline-item" data-aos="zoom-in-down"><FontAwesomeIcon icon={faNpm} /></li>
                     </ul> 
                 </div>
             </section>
